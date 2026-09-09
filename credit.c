78d465030ef626d1aaa333393a298a1dc50c9dab
Ramiel Mancari <ramiel@Ramiels-MacBook-Air.local>
2026-09-09 23:14:43 +0400

    I added luhn algorithm successfully

--- credit.c
+++ credit.c
@@ -4,8 +4,28 @@ int main(void){
     // here we asking the number of card
     printf("Number:");
     scanf("%lld", &card_number);
+    long temp = card_number; 
+    int sum = 0;
+    int position = 0; //calculating the position from the right to the left
+    while (temp > 0){
+        int last_digit = temp % 10; // getting the last number
+        if (position % 2 == 1){
+            // every second number we are multiplying by two
+            int multiplied = last_digit * 2;
+            // now we should find the sum of 2 digit number for example: 14 = 1+4
+            sum += (multiplied / 10) + (multiplied % 10);
+        } else{
+            // one digit number
+            sum += last_digit;
+        }
+        temp /= 10; // cutting the last number
+        position++;// to the next position
 
-    //for cheking now this code will just show me number
-    printf("Received: %lld\n", card_number);
+    }
+    if ( sum % 10 != 0){
+        printf("Invalid\n");
+        return 0; // ending the code
+    }
+    printf("Valid\n");
     return 0;
 }
